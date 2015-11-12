ddc2c6d75006e37be407a4b8cfcd893d5a9f5ed4
gittyhub <junkindabox@gmail.com>
2015-11-12 10:26:08 -0800

    commit for hw 2

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,15 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+	m <<- NULL
+	}
+	get <- fuction() x
+	setinversion <- fuction(solve) i <<- solve
+	getinversion <- function() i
+	list( set=set, get=get, setinversion=setinversion, getinversion=getinversion)
 }
 
 
@@ -12,4 +20,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+		i <- x$getinversion()
+		if(!is.null(i){
+		  message("getting cached data")
+		  return(i)
+		  }
+		data <- x$get()
+		i <- solve(data,...)
+		x$setinversion(i)
+		i
 }
